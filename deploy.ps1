3b6a1204e9737f97f2bcb7068d7cabde2b8fa00d
MyAura Deploy <deploy@myaura.by>
2026-05-03 19:54:37 +0300

    fix(deploy): add .gcloudignore, remove --env-vars-file from deploy, increase timeout to 600s

--- deploy.ps1
+++ deploy.ps1
@@ -1,1 +1,1 @@
-gcloud run deploy myaura --source . --region europe-west1 --platform managed --allow-unauthenticated --memory 4Gi --cpu 2 --concurrency 80 --max-instances 3 --min-instances 0 --timeout 60 --no-cpu-throttling --execution-environment gen2 --env-vars-file .env
+gcloud run deploy myaura --source . --region europe-west1 --platform managed --allow-unauthenticated --memory 4Gi --cpu 2 --concurrency 80 --max-instances 3 --min-instances 0 --timeout 600 --no-cpu-throttling --execution-environment gen2
